4aecee60c1a467eb497cdd663559816296044147
AdiTCG <aditheorigin@gmail.com>
2016-09-28 16:17:19 +0530

    Finding intersection between two array. Method1(Bruteforce)

---
+++ FindIntersection/Bruteforce/Bruteforce/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bruteforce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //populateData
+            Console.WriteLine("Enter the range");
+            var val = Convert.ToInt32(Console.ReadLine());
+
+            int[] in1 = new int[val], in2 = new int[val];
+
+            for (int i = 0; i < val; i++)
+            {
+                in1[i] = i;
+                in2[i] = i + val;
+            }
+            var start = DateTime.Now;
+            Console.WriteLine(Solver.FindIntersection(in1, in2));
+            Console.Write("Time taken: ");
+            Console.WriteLine(DateTime.Now - start);
+        }
+    }
+}
---
+++ FindIntersection/Bruteforce/Bruteforce/Solver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bruteforce
+{
+    static class Solver
+    {
+        internal static int FindIntersection(int[] p_Array1, int[] p_Array2)
+        {
+            int result = 0;
+            for (int i = 0; i < p_Array1.Length; i++)
+            {
+                for (int j = 0; j < p_Array2.Length; j++)
+                {
+                    if (p_Array1[i] == p_Array2[j])
+                    {
+                        ++result;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
