5f0cf79dd9f0dc605461b5441c06b82877c0f260
AdiTCG <aditheorigin@gmail.com>
2016-11-10 20:47:17 +0530

    Introduction of Dynamic Programming
    Using Fibonacci problem to start with

---
+++ DynamicProgramming/Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        //Fibonacci using recursion
+        static int GetFibonacci(int n)
+        {
+            //base case
+            if (n < 3) return 1;
+
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+
+        //Driver program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n:");
+            int n = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine(GetFibonacci(n));
+        }
+    }
+}
