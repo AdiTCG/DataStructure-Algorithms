6f382ff150237e8e77ee588ab688a704626de97d
AdiTCG <aditheorigin@gmail.com>
2016-11-10 23:41:38 +0530

    Removed recursion and using loop. Basically it is doing same without recursion.

--- DynamicProgramming/Fibonacci/Program.cs
+++ DynamicProgramming/Fibonacci/Program.cs
@@ -33,15 +33,38 @@ namespace Fibonacci
             return memory[n];
         }
 
+        static long GetFibonacci(long[] memory, int n)
+        {
+            //base case
+            long r = 0;
+            for (int i = 0; i < n; i++)
+            {
+                r += AuxFibonacci(memory, i);
+            }
+
+            return r;
+        }
+
+        private static long AuxFibonacci(long[] memory, int n)
+        {
+            if (n < 3) return 1;
+
+            if (memory[n] > 0)
+                return memory[n];
+
+            memory[n] = AuxFibonacci(memory, n - 1) + AuxFibonacci(memory, n - 2);
+            return memory[n];
+        }
+
         //Driver program
         static void Main(string[] args)
         {
             Console.WriteLine("Enter n:");
-            int n = Convert.ToInt32( Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
 
             //call
             long[] mem = new long[n + 1];
-            long r = GetFibonacci(n, mem);
+            long r = GetFibonacci(mem, n);
             Console.WriteLine(r);
         }
     }
