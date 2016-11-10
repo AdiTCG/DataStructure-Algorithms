6e91d12de967528d9ea4e3792ab1221cf2f227ef
AdiTCG <aditheorigin@gmail.com>
2016-11-10 21:29:53 +0530

    First step towards Dynamic Programming
    Using momorization to redce repeated execution of sub-problem.

--- DynamicProgramming/Fibonacci/Program.cs
+++ DynamicProgramming/Fibonacci/Program.cs
@@ -8,6 +8,7 @@ namespace Fibonacci
 {
     class Program
     {
+        /*
         //Fibonacci using recursion
         static int GetFibonacci(int n)
         {
@@ -15,6 +16,19 @@ namespace Fibonacci
             if (n < 3) return 1;
 
             return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }*/
+
+        //Memorize sub problem once it is executed.
+        //Calculate each sub problem once
+        static int GetFibonacci(int n, int[] memory)
+        {
+            //base case
+            if (n < 3) return 1;
+            if (memory[n] > 0)
+                return memory[n];
+
+            memory[n] = GetFibonacci(n - 1, memory) + GetFibonacci(n - 2, memory);
+            return memory[n];
         }
 
         //Driver program
@@ -22,7 +36,11 @@ namespace Fibonacci
         {
             Console.WriteLine("Enter n:");
             int n = Convert.ToInt32( Console.ReadLine());
-            Console.WriteLine(GetFibonacci(n));
+
+            //call
+            int[] mem = new int[n + 1];
+            int r = GetFibonacci(n, mem);
+            Console.WriteLine(r);
         }
     }
 }
